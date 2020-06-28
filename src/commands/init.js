a49f9a78a54c4d2eca9db2e29e72d011eb4d525c
Erickmack <erickmomarflores@hotmail.com>
2020-06-27 20:31:47 -0500

    🐛 Fetching bug fix
    Fixes a bug that returned undefined userID's to the database

--- src/commands/init.js
+++ src/commands/init.js
@@ -1,18 +1,20 @@
+const getGameID = require('../TwitchFetch/getGameID')
+
 module.exports = {
   name: 'init',
   description: 'Initialize the bot with your preferences (do this command in the channel you want the bot to send updates)',
   args: true,
   execute(message, args) {
-    const db = require('monk')('localhost:27017/servertest')
+    const db = require('monk')(process.env.URI)
     db.then(() => {
       console.log('connected')
     })
     const collection = db.get('document')
-    collection.find({ ServerID: String(message.guild.id) }).then((res) => {
+    collection.find({ ServerID: String(message.guild.id) }).then(async (res) => {
       let Game = args[0]
       if (!res.length) {
-        // TODO LOOKUP THE ID OF THE GAME PROVIDED
-        collection.insert({ ServerID: String(message.guild.id), Category: Game, Cooldown: '30', ChannelID: String(message.channel.id), Show: 'top', Favorites: [], Banned: [] })
+        let gameID = await getGameID(String(Game))
+        collection.insert({ ServerID: String(message.guild.id), Category: gameID, Cooldown: '5', ChannelID: String(message.channel.id), Show: 'top', Favorites: [], Banned: [] })
           .then((docs) => {
             message.channel.send('You initialized the bot successfully and set the game to ' + Game)
           }).catch((err) => {
