72a1ad3eb7fa1e01297722c73054582c7bc39b87
Erickmack <erickmomarflores@hotmail.com>
2020-07-01 23:07:27 -0500

    🔒 Only admins and creator can use commands
    Only the admins of the server or the owner of the bot can use commands

--- src/index.js
+++ src/index.js
@@ -29,28 +29,29 @@ client.on('ready', () => {
 client.on('message', (message) => {
 
   if (!message.content.startsWith(prefix) || message.author.bot) return;
-  if (!member.hasPermission('ADMINISTRATOR')) return;
-  const args = message.content.slice(prefix.length).split(/ +/);
-  const commandName = args.shift().toLowerCase();
-  const command = client.commands.get(commandName) || client.commands.find(cmd => cmd.aliases && cmd.aliases.includes(commandName));
+  if (message.guild.member(message.author).hasPermission('ADMINISTRATOR') || message.author.id === process.env.CREATOR_ID) {
+    const args = message.content.slice(prefix.length).split(/ +/);
+    const commandName = args.shift().toLowerCase();
+    const command = client.commands.get(commandName) || client.commands.find(cmd => cmd.aliases && cmd.aliases.includes(commandName));
 
 
-  if (!command) return
-  if (command.args && !args.length) {
-    let reply = `You didn't provide any arguments, ${message.author}!`;
-    if (command.usage) {
-      reply += `\nThe proper usage would be: \`${prefix}${command.name} ${command.usage}\``;
+    if (!command) return
+    if (command.args && !args.length) {
+      let reply = `You didn't provide any arguments, ${message.author}!`;
+      if (command.usage) {
+        reply += `\nThe proper usage would be: \`${prefix}${command.name} ${command.usage}\``;
+      }
+
+      return message.channel.send(reply);
     }
 
-    return message.channel.send(reply);
-  }
 
-
-  try {
-    command.execute(message, args, client);
-  } catch (error) {
-    console.error(error);
-    message.reply('there was an error trying to execute that command!');
+    try {
+      command.execute(message, args, client);
+    } catch (error) {
+      console.error(error);
+      message.reply('there was an error trying to execute that command!');
+    }
   }
 });
 
