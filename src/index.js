2a9587dbef18e761bf31cae56b736c12905de799
Erickmack <erickmomarflores@hotmail.com>
2020-07-01 22:56:04 -0500

    🔒 Only Admins can use commands
    Only people with the 'Administrator' permission can use the bot commands

--- src/index.js
+++ src/index.js
@@ -29,7 +29,7 @@ client.on('ready', () => {
 client.on('message', (message) => {
 
   if (!message.content.startsWith(prefix) || message.author.bot) return;
-
+  if (!member.hasPermission('ADMINISTRATOR')) return;
   const args = message.content.slice(prefix.length).split(/ +/);
   const commandName = args.shift().toLowerCase();
   const command = client.commands.get(commandName) || client.commands.find(cmd => cmd.aliases && cmd.aliases.includes(commandName));
