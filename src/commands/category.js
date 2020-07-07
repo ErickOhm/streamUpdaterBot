3be0db05d78d1a4fe482da4cc5884335760f06f3
Erickmack <erickomarflores@hotmail.com>
2020-07-06 22:42:23 -0500

    ✨ Adds channel and category commands
    You can change the channel where the bot sends the updates to and your twitch category for the updates

--- src/commands/category.js
+++ src/commands/category.js
@@ -1,1 +1,27 @@
 // TODO: UPDATE WHAT CATEGORY USERS RECEIVE STREAM UPDATES FROM
+const db = require('monk')(process.env.MONGODB_URI)
+const collection = db.get('document')
+const Discord = require('discord.js');
+const getGameID = require('../TwitchFetch/getGameID')
+
+module.exports = {
+  name: 'category',
+  description: 'Change the category for your updates',
+  usage: '<new category>',
+  args: true,
+  async execute(message, args) {
+    let serverID = message.guild.id
+    let game = args[0]
+    let gameID = await getGameID(String(game))
+    if (gameID) {
+      collection.update({ ServerID: serverID }, { $set: { Category: gameID } }).then(() => {
+        const successMessage = new Discord.MessageEmbed()
+          .setColor('#2ecc71')
+          .setTitle(`Successfully changed your category to ${game}`)
+          .setURL(`https://www.twitch.tv/directory/game/${game}`)
+        message.channel.send(successMessage)
+        db.close()
+      })
+    }
+  },
+};
--- src/commands/channel.js
+++ src/commands/channel.js
@@ -1,1 +1,23 @@
 // TODO COMMAND TO UPDATE THE CHANNEL WHERE THE STREAM UPDATES ARE BEING SENT TO
+const db = require('monk')(process.env.MONGODB_URI)
+const collection = db.get('document')
+
+const Discord = require('discord.js');
+
+module.exports = {
+  name: 'channelUpdate',
+  description: 'Change the channel where you want the bot to send updates (call this command in the new channel)',
+  aliases: 'channel',
+  args: false,
+  execute(message) {
+    let channelID = message.channel.id
+    let serverID = message.guild.id
+    collection.update({ ServerID: serverID }, { $set: { ChannelID: channelID } }).then(() => {
+      const successMessage = new Discord.MessageEmbed()
+        .setColor('#2ecc71')
+        .setTitle(`Successfully changed your channel to ${message.channel.name}`)
+      message.channel.send(successMessage)
+      db.close()
+    })
+  },
+};
