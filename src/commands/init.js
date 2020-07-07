bf3249e522fd1a015087bf5aa16217dd32d55dc7
Erickmack <erickomarflores@hotmail.com>
2020-07-07 07:51:39 -0500

    🎨 Adds success embed message for init command

--- src/commands/init.js
+++ src/commands/init.js
@@ -17,16 +17,24 @@ module.exports = {
           const errorMessage = new Discord.MessageEmbed()
             .setColor('#e74c3c')
             .setTitle('This category doesn\'t exist')
+          db.close()
           return message.channel.send(errorMessage)
         }
         collection.insert({ ServerID: String(message.guild.id), Category: gameID, Cooldown: '5', LastUpdate: new Date(), ChannelID: String(message.channel.id), Show: 'top', Favorites: [], Banned: [] })
           .then((docs) => {
-            message.channel.send('You initialized the bot successfully and set the game to ' + Game)
+            const successMessage = new Discord.MessageEmbed()
+              .setColor('#2ecc71')
+              .setTitle('Successfully initialized the bot and set the game to ' + Game)
+            message.channel.send(successMessage)
           }).catch((err) => {
             message.channel.send(err)
           }).then(() => db.close())
       } else {
-        message.channel.send('You already initialized this server!');
+        const errorMessage = new Discord.MessageEmbed()
+          .setColor('#e74c3c')
+          .setTitle('Server has already been initialized!')
+        message.channel.send(errorMessage);
+        db.close()
       }
     })
   },
