53d9aab20c83e9f57421f7c9bc1c181694c8c03f
Erickmack <erickomarflores@hotmail.com>
2020-07-07 09:48:47 -0500

    ✨ Config command improvement
    config command now collects the category from the user

--- src/commands/init.js
+++ src/commands/init.js
@@ -2,34 +2,43 @@ const getGameID = require('../TwitchFetch/getGameID')
 const Discord = require('discord.js');
 
 module.exports = {
-  name: 'init',
+  name: 'config',
   description: 'Initialize the bot with your preferences (do this command in the channel you want the bot to send updates)',
-  args: true,
-  aliases: ['start'],
-  usage: '!init <category you want updates from>',
+  args: false,
+  aliases: ['start', 'init'],
+  usage: '!init',
   execute(message, args) {
     const db = require('monk')(process.env.MONGODB_URI)
     const collection = db.get('document')
     collection.find({ ServerID: String(message.guild.id) }).then(async (res) => {
-      let Game = args.join(' ')
       if (!res.length) {
-        let gameID = await getGameID(String(Game))
-        if (!gameID.length) {
-          const errorMessage = new Discord.MessageEmbed()
-            .setColor('#e74c3c')
-            .setTitle('This category doesn\'t exist')
-          db.close()
-          return message.channel.send(errorMessage)
-        }
-        collection.insert({ ServerID: String(message.guild.id), Category: gameID, Cooldown: '5', LastUpdate: new Date(), ChannelID: String(message.channel.id), Show: 'top', Favorites: [], Banned: [] })
-          .then((docs) => {
-            const successMessage = new Discord.MessageEmbed()
-              .setColor('#2ecc71')
-              .setTitle('Successfully initialized the bot and set the game to ' + Game)
-            message.channel.send(successMessage)
-          }).catch((err) => {
-            message.channel.send(err)
-          }).then(() => db.close())
+        message.channel.send('What category would you like updates from?').then(() => {
+          const filter = m => message.author.id === m.author.id;
+          message.channel.awaitMessages(filter, { time: 60000, max: 1, errors: ['time'] })
+            .then(async messages => {
+              let Game = messages.first().content
+              let gameID = await getGameID(String(Game))
+              if (!gameID.length) {
+                const errorMessage = new Discord.MessageEmbed()
+                  .setColor('#e74c3c')
+                  .setTitle('This category doesn\'t exist')
+                db.close()
+                return message.channel.send(errorMessage)
+              }
+              collection.insert({ ServerID: String(message.guild.id), Category: gameID, Cooldown: '5', LastUpdate: new Date(), ChannelID: String(message.channel.id), Show: 'top', Favorites: [], Banned: [] })
+                .then((docs) => {
+                  const successMessage = new Discord.MessageEmbed()
+                    .setColor('#2ecc71')
+                    .setTitle('Successfully initialized the bot and set the game to ' + Game)
+                  message.channel.send(successMessage)
+                }).catch((err) => {
+                  message.channel.send(err)
+                }).then(() => db.close())
+            })
+            .catch(() => {
+              message.channel.send('You did not enter any input!');
+            });
+        });
       } else {
         const errorMessage = new Discord.MessageEmbed()
           .setColor('#e74c3c')
