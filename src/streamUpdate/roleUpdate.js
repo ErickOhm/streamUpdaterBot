3fee81e5216f0eb3b7ea697d58ebb8052caf3f31
Erickmack <erickomarflores@hotmail.com>
2020-07-23 16:19:00 -0500

    🚑 Fixes problem with removing roles
    Bot would not remove roles when people stopped streaming, only when they changed games

--- src/streamUpdate/roleUpdate.js
+++ src/streamUpdate/roleUpdate.js
@@ -12,84 +12,75 @@ function roleUpdate(prevState, newState, client, collection) {
         FILTER = res[0].Filter === 'none' ? false : res[0].Filter
       }
     }
-  })
+    if (!CHOSEN_ROLE) return
+    let prevActivity = false
+    let prevActivityGame = 'none'
+    let newActivity = false
+    let newActivityGame = 'none'
 
-  if (!CHOSEN_ROLE) return
-  // filter by game 
-  // activity.state === game
-  let prevActivity = false
-  let prevActivityGame = 'none'
-  let newActivity = false
-  let newActivityGame = 'none'
-
-  newState.activities.forEach(activity => {
-    if (activity.type === ACTIVITY) {
-      newActivity = activity.type
-      newActivityGame = activity.state
-    }
-  })
-  if (prevState) {
-    prevState.activities.forEach(activity => {
+    newState.activities.forEach(activity => {
       if (activity.type === ACTIVITY) {
-        prevActivity = activity.type
-        prevActivityGame = activity.state
+        newActivity = activity.type
+        newActivityGame = activity.state
       }
     })
-  }
+    if (prevState) {
+      prevState.activities.forEach(activity => {
+        if (activity.type === ACTIVITY) {
+          prevActivity = activity.type
+          prevActivityGame = activity.state
+        }
+      })
+    }
 
-  if (newActivity === ACTIVITY) {
-    // check for game filter
-    if (FILTER && (newActivityGame === FILTER) && (prevActivityGame !== newActivityGame)) {
-      let dServer = client.guilds.cache.find(guild => guild.id === newState.guild.id)
-      if (!dServer.roles.cache.find(r => r.name === CHOSEN_ROLE)) { console.log('no such role'); return }
-      let role = dServer.roles.cache.find(r => r.name === CHOSEN_ROLE)
-      let member = dServer.members.cache.find(m => { return m.user.id === newState.userID })
-      try {
-        member.roles.add(role)
-        console.log('added role')
-      } catch (err) {
-        console.log(err)
+    if (newActivity === ACTIVITY) {
+      // check for game filter
+      if (FILTER && (newActivityGame === FILTER) && (prevActivityGame !== newActivityGame)) {
+        let dServer = client.guilds.cache.find(guild => guild.id === newState.guild.id)
+        if (!dServer.roles.cache.find(r => r.name === CHOSEN_ROLE)) { console.log('no such role'); return }
+        let role = dServer.roles.cache.find(r => r.name === CHOSEN_ROLE)
+        let member = dServer.members.cache.find(m => { return m.user.id === newState.userID })
+        try {
+          member.roles.add(role)
+          console.log('added role')
+        } catch (err) {
+          console.log(err)
+        }
+
+      } else if (!FILTER && (!prevActivity || prevActivity !== newActivity)) {
+        // UPDATE ROLE
+        let dServer = client.guilds.cache.find(guild => guild.id === newState.guild.id)
+        if (!dServer.roles.cache.find(r => r.name === CHOSEN_ROLE)) { console.log('no such role'); return }
+        let role = dServer.roles.cache.find(r => r.name === CHOSEN_ROLE)
+        let member = dServer.members.cache.find(m => { return m.user.id === newState.userID })
+        try {
+          member.roles.add(role)
+          console.log('added role')
+        } catch (err) {
+          console.log(err)
+        }
       }
 
-    } else if (!FILTER && (!prevActivity || prevActivity !== newActivity)) {
-      // UPDATE ROLE
+    }
+    if (FILTER && (newActivityGame !== FILTER) && (prevActivity === ACTIVITY)) {
+      // REMOVE ROLE
       let dServer = client.guilds.cache.find(guild => guild.id === newState.guild.id)
-      if (!dServer.roles.cache.find(r => r.name === CHOSEN_ROLE)) { console.log('no such role'); return }
       let role = dServer.roles.cache.find(r => r.name === CHOSEN_ROLE)
       let member = dServer.members.cache.find(m => { return m.user.id === newState.userID })
-      try {
-        member.roles.add(role)
-        console.log('added role')
-      } catch (err) {
-        console.log(err)
+      if(member.roles.cache.some(role => role.name === CHOSEN_ROLE)){
+        member.roles.remove(role)
+        console.log('role removed')
+      }
+    } else if (newActivity !== ACTIVITY) {
+      let dServer = client.guilds.cache.find(guild => guild.id === newState.guild.id)
+      let role = dServer.roles.cache.find(r => r.name === CHOSEN_ROLE)
+      let member = dServer.members.cache.find(m => { return m.user.id === newState.userID })
+      if(member.roles.cache.some(role => role.name === CHOSEN_ROLE)){
+        member.roles.remove(role)
+        console.log('role removed 2')
       }
     }
-
-  }
-  if (FILTER && (newActivityGame !== FILTER)) {
-    // REMOVE ROLE
-    let dServer = client.guilds.cache.find(guild => guild.id === newState.guild.id)
-    let role = dServer.roles.cache.find(r => r.name === CHOSEN_ROLE)
-    let member = dServer.members.cache.find(m => { return m.user.id === newState.userID })
-    try {
-      member.roles.remove(role)
-      console.log('removed role')
-    } catch (err) {
-      console.err(err)
-    }
-  }
-  else if (!FILTER && (prevActivity && prevActivity === ACTIVITY) && (!newActivity || newActivity !== ACTIVITY)) {
-    // REMOVE ROLE
-    let dServer = client.guilds.cache.find(guild => guild.id === newState.guild.id)
-    let role = dServer.roles.cache.find(r => r.name === CHOSEN_ROLE)
-    let member = dServer.members.cache.find(m => { return m.user.id === newState.userID })
-    try {
-      member.roles.remove(role)
-      console.log('removed role')
-    } catch (err) {
-      console.err(err)
-    }
-  }
+  })
 }
 
 module.exports = roleUpdate
