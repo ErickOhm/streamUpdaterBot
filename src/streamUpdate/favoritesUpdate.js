e16a42d3c0b6af5d51f95cb24a1403d4a2ff215e
Erickmack <erickomarflores@hotmail.com>
2020-08-01 22:58:41 -0500

    💄 Modify embed style

--- src/streamUpdate/favoritesUpdate.js
+++ src/streamUpdate/favoritesUpdate.js
@@ -1,61 +1,59 @@
-const getStreamers = require('../TwitchFetch/getStreamers')
-const getStreamerThumbnal = require('../TwitchFetch/getStreamerThumbnail')
-const getGameName = require('../TwitchFetch/getGameName')
-
 const Discord = require('discord.js');
+const getStreamers = require('../TwitchFetch/getStreamers');
+const getStreamerThumbnal = require('../TwitchFetch/getStreamerThumbnail');
+const getGameName = require('../TwitchFetch/getGameName');
 
-module.exports = function (client,collection) {
+module.exports = function (client, collection) {
   collection.find({}).then(async (res) => {
     for (let i = 0; i < res.length; i++) {
-      let favorites = res[i].Favorites
-      let channelID = res[i].ChannelID
-      let favoritesID = []
-      if (!favorites.length) { continue }
-      favorites.forEach(favorite => {
-        favoritesID.push(favorite['ID'])
-      })
-      let streamers = await getStreamers(favoritesID)
-      if (!streamers) continue
+      const favorites = res[i].Favorites;
+      const channelID = res[i].ChannelID;
+      const favoritesID = [];
+      if (!favorites.length) { continue; }
+      favorites.forEach((favorite) => {
+        favoritesID.push(favorite.ID);
+      });
+      const streamers = await getStreamers(favoritesID);
+      if (!streamers) continue;
       if (streamers.data.length > 0) {
         favorites.forEach((favorite) => {
-          let tempData = streamers.data.filter(streamer => {
-            return streamer.user_id === favorite['ID']
-          })
+          const tempData = streamers.data.filter((streamer) => streamer.user_id === favorite.ID);
           if (tempData.length) {
-            if (favorite['wasOnline'] == false) {
-              sendStreamer(tempData[0], client, channelID, collection, res[i].ServerID, favorite['ID'])
+            if (favorite.wasOnline == false) {
+              sendStreamer(tempData[0], client, channelID, collection, res[i].ServerID, favorite.ID);
             }
           } else if (!tempData.length) {
-            collection.update({ 'ServerID': res[i].ServerID, "Favorites.ID": favorite['ID'] }, { $set: { "Favorites.$.wasOnline": false } })
+            collection.update({ ServerID: res[i].ServerID, 'Favorites.ID': favorite.ID }, { $set: { 'Favorites.$.wasOnline': false } });
           }
-        })
+        });
       }
     }
-  })
-}
+  });
+};
 
 async function sendStreamer(data, client, channelID, collection, ServerID, favoriteID) {
-  let thumbnailwidth = data.thumbnail_url.replace('{width}', '320')
-  let thumbnail = thumbnailwidth.replace('{height}', '180')
-  let imagePromise = getStreamerThumbnal(data.user_id)
-  let gameID = data.game_id
-  let gameName = await getGameName(gameID)
-  imagePromise.then(function (img) {
+  const thumbnailwidth = data.thumbnail_url.replace('{width}', '320');
+  const thumbnail = thumbnailwidth.replace('{height}', '180');
+  const imagePromise = getStreamerThumbnal(data.user_id);
+  const gameID = data.game_id;
+  const gameName = await getGameName(gameID);
+  imagePromise.then((img) => {
     const streamerEmbed = new Discord.MessageEmbed()
       .setColor('#8e44ad')
       .setTitle(`${data.title}`)
       .setURL(`https://twitch.tv/${data.user_name}`)
       .setAuthor(data.user_name, img, `https://twitch.tv/${data.user_name}`)
       .setThumbnail(img)
-      .addFields({ name: 'Playing', value: gameName, inline: true }, { name: 'Lang', value: data.language, inline: true }, { name: 'Viewers', value: data.viewer_count })
+      .addFields({ name: 'Playing', value: gameName, inline: true }, { name: 'Language', value: data.language, inline: true }, { name: 'Viewers', value: data.viewer_count })
       .setImage(thumbnail)
+      .setTimestamp();
     try {
-      client.channels.fetch(channelID).then(channel => {
-        channel.send(`${data.user_name} is live!`, streamerEmbed)
-      })
+      client.channels.fetch(channelID).then((channel) => {
+        channel.send(`${data.user_name} is live!`, streamerEmbed);
+      });
     } catch (error) {
-      console.error(channelID, error)
+      console.error(channelID, error);
     }
-  })
-  collection.update({ 'ServerID': ServerID, "Favorites.ID": favoriteID }, { $set: { "Favorites.$.wasOnline": true } })
+  });
+  collection.update({ ServerID, 'Favorites.ID': favoriteID }, { $set: { 'Favorites.$.wasOnline': true } });
 }
