920028da0233d28cc9ef926341bbb3e67f08ead5
Erick F <erickmack1998@gmail.com>
2021-01-17 18:23:36 -0600

    added api call command

---
+++ src/commands/makeAPIcall.js
@@ -0,0 +1,30 @@
+const unirest = require("unirest");
+const Discord = require('discord.js')
+
+module.exports = {
+  name: "call",
+  description: "call an api endpoint",
+  usage: "!call <endpoint>",
+  execute(message, args, client) {
+    if (message.author.id !== process.env.CREATOR_ID) return;
+    const URL = args[0];
+    let req = unirest("GET", URL)
+      .then(function (res) {
+        if (res.error) {
+          return res.error;
+        }
+        let parsed = JSON.parse(res.raw_body);
+        console.log(parsed)
+        const result = new Discord.MessageEmbed()
+          .setColor('#f1c40f')
+          .setTitle(`Called made successfully`)
+          .setDescription(JSON.stringify(parsed))
+        try {
+          message.channel.send(result)
+        } catch (error) {
+          console.error(error, message.channel)
+        }
+      });
+    return req;
+  },
+};
