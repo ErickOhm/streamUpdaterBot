88b2b3aeabb0f1f9921e74f31269cd757cba74af
Erick F <erickmack1998@gmail.com>
2021-01-23 23:39:02 -0600

    update remove role condition

--- src/streamUpdate/checkRemoveRole.js
+++ src/streamUpdate/checkRemoveRole.js
@@ -21,7 +21,7 @@ function checkRole(client,collection) {
           if(isStreaming === false){
             member.roles.remove(guildRole)
             console.log(member.user.username)
-            console.log(member.roles.cache,find('name','Now Live'))
+            console.log(member.roles.cache,find(role => role.name === guildRole))
           }
           if(Filter && (game !== Filter)){
             // remove role
