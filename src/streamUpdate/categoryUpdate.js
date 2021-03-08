722b5674703ba4e0621d2d86ab835b72e2dac0a9
Erick F <erickmack1998@gmail.com>
2021-03-07 21:13:59 -0600

    error fixing

--- src/streamUpdate/categoryUpdate.js
+++ src/streamUpdate/categoryUpdate.js
@@ -1,88 +1,88 @@
-const getStreams = require("../TwitchFetch/getStreams");
-const getImg = require("../TwitchFetch/getStreamerThumbnail");
-const getGameName = require("../TwitchFetch/getGameName");
-const getLogin = require("../TwitchFetch/getStreamerInfo");
-const Discord = require("discord.js");
+const Discord = require('discord.js');
+const getStreams = require('../TwitchFetch/getStreams');
+const getImg = require('../TwitchFetch/getStreamerThumbnail');
+const getGameName = require('../TwitchFetch/getGameName');
+const getLogin = require('../TwitchFetch/getStreamerInfo');
 
 module.exports = function (client, collection) {
   collection.find({}).then(async (res) => {
     for (let i = 0; i < res.length; i++) {
-      let category = res[i].Category;
-      if (category == "none") {
+      const category = res[i].Category;
+      if (category == 'none') {
         continue;
       }
-      let LastUpdateSec = res[i].LastUpdate.getTime() / 1000;
-      let cooldownSec = +res[i].Cooldown * 60;
-      let cooldownTime = Math.round(LastUpdateSec + cooldownSec);
-      let currentTime = new Date().getTime() / 1000;
+      const LastUpdateSec = res[i].LastUpdate.getTime() / 1000;
+      const cooldownSec = +res[i].Cooldown * 60;
+      const cooldownTime = Math.round(LastUpdateSec + cooldownSec);
+      const currentTime = new Date().getTime() / 1000;
       if (currentTime >= cooldownTime) {
-        let streams = await getStreams(category);
+        const streams = await getStreams(category);
         if (!streams.data.length) continue;
-        let data = streams.data;
-        let channelID = res[i].ChannelID;
+        const { data } = streams;
+        const channelID = res[i].ChannelID;
         try {
           client.channels.fetch(channelID).then((channel) => {
             channel.messages.fetch({ limit: 15 }).then((messages) => {
-              let messageArray = messages.array();
-              let usernameArray = [];
+              const messageArray = messages.array();
+              const usernameArray = [];
               messageArray.forEach((msg) => {
                 if (msg.author.bot) {
-                  let name = msg.content.split(" ")[0];
+                  const name = msg.content.split(' ')[0];
                   usernameArray.push(name);
                 }
               });
-              let mostViewed = data[0];
-              let index = usernameArray.indexOf(mostViewed.user_name);
+              const mostViewed = data[0];
+              const index = usernameArray.indexOf(mostViewed.user_name);
               if (index == -1) {
                 sendStreamer(
                   mostViewed,
                   client,
                   channelID,
                   collection,
-                  res[i].ServerID
+                  res[i].ServerID,
                 );
               } else if (index >= 0) {
                 if (data.length > 1) {
-                  let rand = Math.floor(Math.random() * data.length + 1);
-                  let randStreamer = data[rand];
-                  let index = usernameArray.indexOf(randStreamer.user_name);
+                  const rand = Math.floor(Math.random() * data.length + 1);
+                  const randStreamer = data[rand];
+                  if(!randStreamer) return
+                  const index = usernameArray.indexOf(randStreamer.user_name);
                   if (index < 0) {
                     sendStreamer(
                       randStreamer,
                       client,
                       channelID,
                       collection,
-                      res[i].ServerID
+                      res[i].ServerID,
                     );
                   } else {
                     messageArray.forEach((msg) => {
-                      if (msg.content.split("")[0] === randStreamer.user_name) {
-                        let currentDate = new Date();
-                        let oneHour = 3_600_000;
+                      if (msg.content.split('')[0] === randStreamer.user_name) {
+                        const currentDate = new Date();
+                        const oneHour = 3600000;
                         if (msg.createdTimestamp + oneHour < currentDate) {
                           sendStreamer(
                             randStreamer,
                             client,
                             channelID,
                             collection,
-                            res[i].ServerID
+                            res[i].ServerID,
                           );
                         }
-                        return
                       }
                     });
                   }
                 }
-                let messageCooldown = LastUpdateSec - cooldownSec * index;
-                let timeDiff = currentTime - messageCooldown;
-                let threeHours = 3 * 60 * 60;
+                const messageCooldown = LastUpdateSec - cooldownSec * index;
+                const timeDiff = currentTime - messageCooldown;
+                const threeHours = 3 * 60 * 60;
                 if (timeDiff > threeHours) {
                   sendStreamer(
                     data,
                     client,
                     channelID,
                     collection,
-                    res[i].ServerID
+                    res[i].ServerID,
                   );
                 }
               }
@@ -97,23 +97,23 @@ module.exports = function (client, collection) {
 };
 
 async function sendStreamer(data, client, channelID, collection, ID) {
-  let thumbnailwidth = data.thumbnail_url.replace("{width}", "320");
-  let thumbnail = thumbnailwidth.replace("{height}", "180");
-  let imagePromise = getImg(data.user_id);
-  let userLogin = await getLogin(data.user_id);
-  let gameID = data.game_id;
-  let gameName = await getGameName(gameID);
-  imagePromise.then(function (img) {
+  const thumbnailwidth = data.thumbnail_url.replace('{width}', '320');
+  const thumbnail = thumbnailwidth.replace('{height}', '180');
+  const imagePromise = getImg(data.user_id);
+  const userLogin = await getLogin(data.user_id);
+  const gameID = data.game_id;
+  const gameName = await getGameName(gameID);
+  imagePromise.then((img) => {
     const streamerEmbed = new Discord.MessageEmbed()
-      .setColor("#8e44ad")
+      .setColor('#8e44ad')
       .setTitle(`${data.title}`)
       .setURL(`https://twitch.tv/${userLogin}`)
       .setAuthor(data.user_name, img, `https://twitch.tv/${data.user_name}`)
       .setThumbnail(img)
       .addFields(
-        { name: "Playing", value: gameName, inline: true },
-        { name: "Language", value: data.language, inline: true },
-        { name: "Viewers", value: data.viewer_count }
+        { name: 'Playing', value: gameName, inline: true },
+        { name: 'Language', value: data.language, inline: true },
+        { name: 'Viewers', value: data.viewer_count },
       )
       .setImage(thumbnail)
       .setTimestamp();
